fbccc53ae01e8d3aa98fd851daf96e3c9994f3a1
AntonioDelCas <antoniojdelgado@yahoo.es>
2015-06-10 16:34:43 +0200

    Hipermedia

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,16 @@
+// GET /quixes/question
+exports.question = function(reg, res){
+    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+
+// GET /quixes/answer
+exports.answer = function(reg, res){
+    if(reg.query.respuesta === 'Roma'){
+        res.render('quizes/answer', {respuesta: 'Correcto'});
+    } else {
+        res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    }
+
+};
+
