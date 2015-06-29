78646c23f5ecb8ea7b8fdde717a1173a25997450
AntonioDelCas <antoniojdelgado@yahoo.es>
2015-06-29 11:25:34 +0200

    bd en heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,32 +1,28 @@
-var models = require('../models/model.js');
+var models = require('../models/models.js');
 
-// GET /quixes/question
-exports.question = function(reg, res){
-    models.Quiz.findAll().success(function(quiz){
-        res.render('quizes/question', {pregunta: quiz[0].pregunta});
+// GET /quizes
+exports.index = function(req, res) {
+    models.Quiz.findAll().then(function(quizes) {
+        res.render('quizes/index.ejs', { quizes: quizes});
     })
-    //res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
+// GET /quizes/:id
+exports.show = function(req, res) {
+    models.Quiz.find(req.params.quizId).then(function(quiz) {
+        res.render('quizes/show', { quiz: quiz});
+    })
+};
 
-// GET /quixes/answer
-exports.answer = function(reg, res){
-    models.Quiz.findAll().success(function(quiz){
-        if(reg.query.respuesta === quiz[0].respuesta){
-            res.render('quizes/answer', {respuesta: 'Correcto'});
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+    models.Quiz.find(req.params.quizId).then(function(quiz) {
+        if (req.query.respuesta === quiz.respuesta) {
+            res.render('quizes/answer',
+                { quiz: quiz, respuesta: 'Correcto' });
         } else {
-            res.render('quizes/answer', {respuesta: 'Incorrecto'});
+            res.render('quizes/answer',
+                { quiz: quiz, respuesta: 'Incorrecto'});
         }
-
     })
-
-    /*if(reg.query.respuesta === 'Roma'){
-        res.render('quizes/answer', {respuesta: 'Correcto'});
-    } else {
-        res.render('quizes/answer', {respuesta: 'Incorrecto'});
-    }*/
-
-
 };
-
-
--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,14 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
     res.render('index', { title: 'Quiz' });
 });
 
-/* GET /quizes/author. */
-router.get('/author', function(req, res) {
-    res.render('author', {autor: 'Antonio Delgado'});
-});
-
-/* GET /quizes/question. */
-router.get('/quizes/question', quizController.question);
-
-/* GET /quizes/answer. */
-router.get('/quizes/answer', quizController.answer);
-
-
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
