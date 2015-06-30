a638f9be71d3f1bc0bc7d34227843c509e6989e8
AntonioDelCas <antoniojdelgado@yahoo.es>
2015-06-30 13:46:58 +0200

    Despliegue de Autolad 2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,5 @@
 var models = require('../models/models.js');
 
-
 // Autoload -factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
     models.Quiz.find(quizId).then(
@@ -17,8 +16,9 @@ exports.load = function(req, res, next, quizId){
 
 // GET /quizes
 exports.index = function(req, res){
-    models.Quiz.findAll().then(function(quizes){
-        res.render('quizes/index.ejs', {quizes: quizes});
+    models.Quiz.findAll().then(
+        function(quizes){
+            res.render('quizes/index', {quizes: quizes});
     }).catch(function(error){next(error);})
 };
 
