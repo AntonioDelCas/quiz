d7ea30f0bba483686205f0a7f8edcc99ef0901f1
AntonioDelCas <antoniojdelgado@yahoo.es>
2015-06-30 22:44:24 +0200

    Añadiendo datos a bd

--- models/models.js
+++ models/models.js
@@ -47,6 +47,21 @@ sequelize.sync().then(function() {
             Quiz.create({   pregunta: 'Capital de Italia',
                 respuesta: 'Roma'
             });
+            Quiz.create({   pregunta: 'Capital de España',
+                respuesta: 'Madrid'
+            });
+            Quiz.create({   pregunta: 'Capital de Francia',
+                respuesta: 'París'
+            });
+            Quiz.create({   pregunta: 'Capital de Grecia',
+                respuesta: 'Atenas'
+            });
+            Quiz.create({   pregunta: 'Capital de Alemania',
+                respuesta: 'Berlín'
+            });
+            Quiz.create({   pregunta: 'Capital de Reino Unido',
+                respuesta: 'Londres'
+            });
             Quiz.create({   pregunta: 'Capital de Portugal',
                 respuesta: 'Lisboa'
             })
